04d65c5d27e078cd361f367dadf1fa68b1c13247
sameergautam <sameergautam@outlook.com>
2013-07-14 23:28:33 +0545

    add end in each of the if-else block

--- ex31.rb
+++ ex31.rb
@@ -6,7 +6,7 @@ puts "You enter a dark room with two doors. Do you go through door #1 or door #2
 
 prompt; door = gets.chomp
 
-if door == 1
+if door == "1"
 	puts "There's a giant bear eating a cheese cake. What do you do?"
 	puts "1. Take the cake."
 	puts "2. Scream at the bear."
@@ -19,7 +19,7 @@ if door == 1
 		puts "The bear eats your legs off. Good job!"
 	else
 		puts "Well, doing #{bear} is probably better. Bear runs away."
-
+	end
 elsif door == "2"
 	puts "You stare into the endless abyss at Cthuhlu's retina"
 	puts "1. Blueberries"
@@ -32,7 +32,7 @@ elsif door == "2"
 		puts "Your body survives powered by a mind of jello. Good job!"
 	else
 		puts "The insanity rots your eyes into a pool of muck Good job!"
+	end
 else
 	puts "You stumble around and fall on a knife and die. Good job!"
 end
-		
